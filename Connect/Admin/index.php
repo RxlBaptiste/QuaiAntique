3a4d3e8e5e0a9bff8ceea7bf3747f8fa03193e6f
RxlBaptiste <86966587+RxlBaptiste@users.noreply.github.com>
2023-02-16 11:46:54 +0100

    ConnexionAdmin

---
+++ Connect/Admin/index.php
@@ -0,0 +1,29 @@
+<?php
+
+  if(session_status() == '1'){
+    session_start();
+    if(!$_SESSION['Admin']){
+      echo 'FLOW !!';
+      header('location: ../Logout.php'); 
+    }
+  }
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin</title>
+</head>
+
+<body>
+
+    <a href="../Logout.php">Se deconnecter</a>
+    <p>FLOW !!</p>
+</body>
+
+</html>
--- Connect/connection.php
+++ Connect/connection.php
@@ -3,51 +3,69 @@
 session_start();
 
 $dbClient = new PDO('mysql:host=localhost;dbname=lequaiantique;', 'root', '');
-
-if(isset($_POST['submit'])){
-    $mail = $_POST['email'];
-    $pass = $_POST['password'];
 /* 
-    $mdpSaisi = htmlspecialchars($_POST['password']); */
+if(isset($_POST['submit'])){
+    
+} */
+if(isset($_POST['submit'])){/* 
+    if(!empty($_POST['email']) AND !empty($_POST['password'])){ */
+        $pseudoParDefaut = "admin";
+        $mdpParDefaut = "Admin_22";
 
-    $sqlPassClient = $dbClient->query("SELECT * FROM client WHERE mail = '$mail'");
-    while ($passSession =$sqlPassClient->fetch()){
-        $passWordSession = $passSession['password'];
-        echo $passWordSession;
-        echo "<br>";
-        echo $pass;
-        echo "<br>";
-        echo "<br>";
-    }
+        $pseudoSaisi = htmlspecialchars($_POST['email']);
+        $mdpSaisi = htmlspecialchars($_POST['password']);
+
+        if($pseudoSaisi == $pseudoParDefaut AND $mdpSaisi == $mdpParDefaut){
+            $_SESSION['Admin'] = $mdpSaisi;
+            header('location: Admin/index.php');
+        } else{
+            /*echo "Mot de passe ou pseudo incorrect";
+        } 
+    }*/
+
+        $mail = $_POST['email'];
+        $pass = $_POST['password'];
+    /* $mdpSaisi = htmlspecialchars($_POST['password']); */
+
+        $sqlPassClient = $dbClient->query("SELECT * FROM client WHERE mail = '$mail'");
+        while ($passSession =$sqlPassClient->fetch()){
+            $passWordSession = $passSession['password'];
+            echo $passWordSession;
+            echo "<br>";
+            echo $pass;
+            echo "<br>";
+            echo "<br>";
+        }
 
      
-    $sqlSelect = "SELECT * FROM client where mail = '$mail'";
-    $sqlPrepare = $dbClient->prepare($sqlSelect);
-    $sqlPrepare->execute();
+        $sqlSelect = "SELECT * FROM client where mail = '$mail'";
+        $sqlPrepare = $dbClient->prepare($sqlSelect);
+        $sqlPrepare->execute();
     
-    if(!isset($_SESSION['mail'])){
-        $_SESSION['mail'] = $mail;
-    }
-    $sqlClient = $dbClient->query("SELECT * FROM client WHERE mail = '$mail'");
-     while ($session = $sqlClient->fetch()){
+        if(!isset($_SESSION['mail'])){
+            $_SESSION['mail'] = $mail;
+        }
+        $sqlClient = $dbClient->query("SELECT * FROM client WHERE mail = '$mail'");
+        while ($session = $sqlClient->fetch()){
        
-        $mailSession = $session['mail'];
-        $passSession = $session['password'];
-    }
-    echo $mailSession;
-    echo "<br>";
-    echo $mail; 
-    if($mailSession == $mail){
-        if(password_verify($pass,$passWordSession)){
-            header("Location:Accueil.php" );
-            echo "Le mot de passe est validé";
+            $mailSession = $session['mail'];
+            $passSession = $session['password'];
+        }
+        echo $mailSession;
+        echo "<br>";
+        echo $mail; 
+        if($mailSession == $mail){
+            if(password_verify($pass,$passWordSession)){
+                header("Location:Accueil.php" );
+                echo "Le mot de passe est validé";
+            }else{
+                echo "Le mot de passe n'est pas vérifié";
+                header("Location: ../acces/creation/inscription.html");
+            }
         }else{
-            echo "Le mot de passe n'est pas vérifié";
+            echo"Le mot de passe n'est pas vérifié";
             header("Location: ../acces/creation/inscription.html");
         }
-    }else{
-        echo"Le mot de passe n'est pas vérifié";
-        header("Location: ../acces/creation/inscription.html");
     }
 }
 ?>
