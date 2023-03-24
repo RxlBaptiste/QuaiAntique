5528c572941a9ef5552482b2ff08a395c96d6ab9
RxlBaptiste <86966587+RxlBaptiste@users.noreply.github.com>
2023-03-24 15:36:32 +0100

    EndPageReservation

--- Pages/bonDeResa.php
+++ Pages/bonDeResa.php
@@ -4,36 +4,110 @@ if ( isset( $_POST['submit'] ) ) {
         $date = $_POST['date'];
         $commentaire = $_POST['commentaire'];
         $horaire = $_POST['horaires'];
-        $allergies = $_POST["allergies"];
+        $allergies = $_POST["allergies"];  
+    }
+        if(!empty($allergies)){
+            $count = -1;
+            foreach ($allergies as $allergie) {
+                $allergy[] = /* "<br>" . */"  - " .  $allergie;
+                $count++;
 
-        echo "Allergies sélectionnées : ";
-        foreach ($allergies as $allergie) {
-            echo "<br>" . "- " . $allergie;
+                if($count == 0){
+                    $allergie1 = $allergy[$count];
+                }
+                if($count == 1){
+                    $allergie2 = $allergy[$count];
+                }
+                if($count == 2){
+                    $allergie3 = $allergy[$count];
+                }
+                if($count == 3){
+                    $allergie4 = $allergy[$count];
+                } 
+                if($count == 4){
+                    $allergie5 = $allergy[$count];
+                } 
+                if($count == 5){
+                    $allergie6 = $allergy[$count];
+                } 
+                if($count == 6){
+                    $allergie7 = $allergy[$count];
+                } 
+                if($count == 7){
+                    $allergie8 = $allergy[$count];
+                } 
+                if($count == 8){
+                    $allergie9 = $allergy[$count];
+                } 
+                if($count == 9){
+                    $allergie10 = $allergy[$count];
+                } 
+                if($count == 10){
+                    $allergie11 = $allergy[$count];
+                } 
+                if($count == 11){
+                    $allergie12 = $allergy[$count];
+                } 
+                if($count == 12){
+                    $allergie13 = $allergy[$count];
+                } 
+                if($count == 13){
+                    $allergie14 = $allergy[$count];
+                } 
+                if($count == 14){
+                    $allergie15 = $allergy[$count];
+                } 
+                if($count == 15){
+                    $allergie16 = $allergy[$count];
+                } 
+                if($count == 16){
+                    $allergie17 = $allergy[$count];
+                } 
+                if($count == 17){
+                    $allergie18 = $allergy[$count];
+                } 
+                if($count == 18){
+                    $allergie19 = $allergy[$count];
+                } 
+                if($count == 19){
+                    $allergie20 = $allergy[$count];
+                } 
+            }
         }
-
-        /* $dbClient = new PDO('mysql:host=localhost;dbname=lequaiantique;', 'root', '');
+        echo "<br>";
+        var_dump($allergy);
+    $switch = 0;
+   $dbClient = new PDO('mysql:host=localhost;dbname=lequaiantique;', 'root', '');
     
-        $sqlInsert = "INSERT INTO reservation 
-        (
-            NbPers,
-            Date,
-            Commentaire,
-            Horaire
-            Allergies
+    $sqlInsert = "INSERT INTO reservation 
+    (
+        Allergies
+    ) 
+    VALUES
+    (
+        '$allergie1'
+        '$allergie2'
+        '$allergie3'
+        '$allergie4'
+        '$allergie5'
+        '$allergie6'
+        '$allergie7'
+        '$allergie8'
+        '$allergie9'
+        '$allergie10'
+        '$allergie11'
+        '$allergie12'
+        '$allergie13'
+        '$allergie14'
+        '$allergie15'
+        '$allergie16'
+        '$allergie17'
+        '$allergie18'
+        '$allergie19'
+        '$allergie20'
+    )";
+    $sqlPrepareInsert = $dbClient->prepare($sqlInsert);
+    $sqlPrepareInsert->execute();
 
-        ) 
-        VALUES
-        (
-            '$nbPers',
-            '$date',
-            '$commentaire',
-            '$horaire',
-            '$allergies'
-        )";
-        $sqlPrepareInsert = $dbClient->prepare($sqlInsert);
-        $sqlPrepareInsert->execute(); */
-         
-}
-
-
+    header("Location: ../accueilPopUp.html");
 ?>
